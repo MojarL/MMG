008c8d2d599ad23ef6a16c07cfedcda3df2bc56e
MojarL <megamusicagratis@gmail.com>
2020-08-20 14:05:45 -0300

    agregado disqus, ads y mas bandas.

--- call_function.js
+++ call_function.js
@@ -15,9 +15,8 @@ class Imported{
 let tabla = new Imported;
 tabla.importHtml('bandsTable/bandsTable.html', 'content');
 let disqus = new Imported;
-disqus.importHtml('disqus/disqus.html','footer');
-let disqusScript = new Imported;
-disqusScript.importHtml('disqus/disqusScript.html','dropdown-content');
+disqus.importHtml('../disqus/disqus.html','footer');
+
 
 
 function call_function(){
@@ -27,3 +26,23 @@ function call_function(){
 }
 
 call_function();
+
+//DISQUS----------------
+function init() {
+    var disqus_config = function () {
+        this.page.url = document.URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = document.identifier; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    (function () { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://megamusicagratis-com.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+
+}
+window.onload = init();
+
+
+//all links open new blank
+$("a").attr("target", "_blank");
